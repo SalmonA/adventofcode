a115798ca692a82ecc6191feb250193e99c28459
Alex Salmon <salmon.alexpaul@gmail.com>
2022-12-05 09:18:11 +0100

    Day 5

--- 2022/03/03.py
+++ 2022/03/03.py
@@ -4,7 +4,6 @@ with open('input.txt', 'r') as f:
 priorities = 0
 
 for groupIndex in range(len(input)//3):
-    print(groupIndex)
     group = input[groupIndex*3:groupIndex*3+3]
     for c in group[0]:
         if c in group[1] and c in group[2]:
