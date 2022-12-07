5ec36dc3d52d048a8e8536fdd024b831e72306c3
Alex Salmon <salmon.alexpaul@gmail.com>
2022-12-07 03:46:00 +0100

    Day 6

---
+++ 2022/06/06.py
@@ -0,0 +1,13 @@
+with open('input.txt', 'r') as f:
+    message = f.readline()
+
+# marker_length = 4
+marker_length = 14
+
+for i in range(len(message)):
+    if len(set(message[i:i + marker_length])) == marker_length:
+        print(i + marker_length)
+        break
+      
+        
+       
